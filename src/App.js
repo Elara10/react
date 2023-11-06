41094f1625e9e6ab0319a21fae45d061548839bc
Ishakumari10 <isha02740@gmail.com>
2023-11-06 10:18:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+let name="Harry";
+function App() {
+  return (
+ <>
+ <nav>
+  <ul>
+  <li>Home</li>
+  <li>About</li>
+  <li>Contact</li>
+  </ul>
+ </nav>
+ <div className="container"></div>
+ <h1>Hello {name}</h1>
+ <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore nobis exercitationem non pariatur, nesciunt soluta quibusdam deleniti veniam quia reprehenderit, rerum sunt temporibus ducimus natus esse sed tempore ipsa minus obcaecati laboriosam, ipsum nam.</p>
+ </>
+  );
+}
+
+export default App;
