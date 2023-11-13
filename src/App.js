d1e033b696c8c35a580bb5a740d2cfbb2d9db93b
Ishakumari10 <isha02740@gmail.com>
2023-11-13 22:04:03 +0530

    Lec-12 completed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
+import React,{useState} from 'react'
+
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+    setMode('dark');
+    document.body.style.backgroundColor='#042743';
+  }
+  else{
+  setMode('light');
+  document.body.style.backgroundColor='white';
+  }
+}
   return (
   <>
  
- <Navbar title="TextUtils" />
+ <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
  <div className="container my-3">
- <TextForm heading="Enter the text to analyze below"/>
+ <TextForm heading="Enter the text to analyze below" mode={mode}/>
  <About/>
  </div>
  
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -35,10 +35,10 @@ export default function TextForm(props) {
 
   return (
     <>
-    <div className="container">
+    <div className="container"  style={{color:props.mode==='dark' ? 'white' : 'black'}}>
   <h1>{props.heading}</h1>
 <div className="mb-3">
-<textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+<textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark' ? 'grey' : 'white',color:props.mode==='dark' ? 'white' : 'black'}}  id="myBox" rows="8"></textarea>
 </div>
 <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
 <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
@@ -47,13 +47,13 @@ export default function TextForm(props) {
 <button className="btn btn-primary mx-2" onClick={handleExtraspaces}>Remove extra spaces</button>
 
 </div>
-<div className="container my-2">
+<div className="container my-2" style={{color:props.mode==='dark' ? 'white' : 'black'}}>
 <h2>Your text summary</h2>
 <p>{text.split(" ").length} words and {text.length} characters</p>
 <p>{0.008 * text.split(" ").length}Minutes read</p>
 <h2>Preview</h2>
-<p>{text}</p>
+<p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
 </div>
-    </>
+</>
   )
 }
