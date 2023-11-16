511a759172b47defcee95693d1ba29a3f6dca286
Ishakumari10 <isha02740@gmail.com>
2023-11-16 15:06:43 +0530

    lec 17 completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
 import React,{useState} from 'react'
 import Alert from './components/Alert';
 import { BrowserRouter as Router,Switch,Route,Link } from 'react-router-dom';
@@ -45,21 +45,20 @@ else{
 }
   return (
   <>
- <Router>
+ {/* <Router> */}
  <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
  <Alert alert={alert}/>
  <div className="container my-3">
-  <Switch>
-    <Route exact path="/about">
+  {/* <Switch> */}
+    {/* <Route exact path="/about">
       <About/>
-      </Route>
-      <Route exact path="/">
+      </Route> */}
+      {/* <Route exact path="/"> */}
  <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-      </Route>
-
-  </Switch>
+      {/* </Route> */}
+  {/* </Switch> */}
  </div>
-  </Router>
+  {/* </Router> */}
  
   </>
   );
