baaae91ee08f438cc88e207227575f40611470b6
Ishakumari10 <isha02740@gmail.com>
2023-11-17 12:26:10 +0530

    lec 19

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import React,{useState} from 'react'
 import Alert from './components/Alert';
 import { BrowserRouter as Router,Switch,Route,Link } from 'react-router-dom';
@@ -26,40 +26,40 @@ function App() {
     setMode('dark');
     document.body.style.backgroundColor='#042743';
     showAlert("Dark mode has been enabled","success");
-    document.title='TextUtils-Dark Mode';
-    setInterval(() => {
-      document.title='TextUtils is Amazing Mode';
-    }, 2000);
+  //   document.title='TextUtils-Dark Mode';
+  //   setInterval(() => {
+  //     document.title='TextUtils is Amazing Mode';
+  //   }, 2000);
   
-  setInterval(() => {
-    document.title='Install TextUtils Now';
-  }, 1500);
+  // setInterval(() => {
+  //   document.title='Install TextUtils Now';
+  // }, 1500);
 }
 else{
   setMode('light');
   document.body.style.backgroundColor='white';
   showAlert("Light mode has been enabled","success");
-  document.title='TextUtils-Light Mode';
+  // document.title='TextUtils-Light Mode';
 
   }
 }
   return (
   <>
- {/* <Router> */}
+ <Router>
  <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
  <Alert alert={alert}/>
  <div className="container my-3">
-  {/* <Switch> */}
-    {/* <Route exact path="/about">
-      <About/>
-      </Route> */}
-      {/* <Route exact path="/"> */}
- <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-      {/* </Route> */}
-  {/* </Switch> */}
+  <Switch>
+    <Route exact path="/about">
+      <About mode={mode}/>
+      </Route> */
+       <Route exact path="/">
+ <TextForm showAlert={showAlert} heading="Try TextUtils-Word Counter,Character Counter,Remove extra Spaces" mode={mode}/>
+      </Route>
+  </Switch>
   {/*  */}
  </div>
-  {/* </Router> */}
+   </Router> 
  
   </>
   );
