804c72958a19dfc39d916d21f4b49d4f87c5b68b
Ishakumari10 <isha02740@gmail.com>
2023-11-17 12:26:28 +0530

    lec 19

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TextForm from './components/TextForm';
 import About from './components/About';
 import React,{useState} from 'react'
 import Alert from './components/Alert';
-import { BrowserRouter as Router,Switch,Route,Link } from 'react-router-dom';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
 
 
 function App() {
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -42,7 +42,7 @@ export default function TextForm(props) {
             // console.log("On change");
             setText(event.target.value);
           }
-    const[text,setText]=useState('');
+    const[text,setText]=useState("");
 
   return (
     <>
@@ -61,7 +61,7 @@ export default function TextForm(props) {
 <div className="container my-2" style={{color:props.mode==='dark' ? 'white' : 'black'}}>
 <h2>Your text summary</h2>
 <p>{text.split(" ").filter((element)=>{return element.length!==0}).
-  length} words and {text.length} characters</p>
+ length} words and {text.length} characters</p>
 <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
 <h2>Preview</h2>
 <p>{text.length>0?text:"Nothing to preview"}</p>
